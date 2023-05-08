53d7ae080ea699c7adfc94f0aa007b31c737a36d
gongboo <53379511+gongboo@users.noreply.github.com>
2023-05-08 14:37:02 +0900

    gpt api 추가
    gpt 사용을 위한 함수가 추가되었습니다.
    작동을 위해서 .env 파일을 추가해야합니다.
    
    .env 내용
    SECRET_KEY="시크릿 키"

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+### from gpt_api import getEmotionFromGPT # test
 
 import config
 db = SQLAlchemy()
@@ -9,6 +11,9 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+
+    # load env
+    load_dotenv()
 
     # ORM
     db.init_app(app)
---
+++ gpt_api.py
@@ -0,0 +1,67 @@
+import os
+import openai
+
+openai.api_key = os.environ.get('SECRET_KEY')
+
+### test text
+text="People were screaming in the darkness, some crying and some praying. The snowflake shined under sunlight.\n\n###\n\n"
+
+def getEmotionFromGPT(text):
+    response = openai.Completion.create(
+        model="ada:ft-personal-2023-05-03-15-51-52",
+        prompt=text,
+        temperature=0,
+        max_tokens=1,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        logprobs=5,
+        presence_penalty=0.0
+    )
+    result=response["choices"][0]["text"].strip()
+    
+    return result
+
+def getLogProbEmotionFromGPT(text):
+    response = openai.Completion.create(
+        model="ada:ft-personal-2023-05-03-15-51-52",
+        prompt=text,
+        temperature=0,
+        max_tokens=1,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        logprobs=5,
+        presence_penalty=0.0
+    )
+    result=response["choices"][0]["logprobs"]["top_logprobs"]
+    
+    return result
+
+
+def getWeatherFromGPT(text):
+    response = openai.Completion.create(
+        model="ada:ft-personal-2023-05-04-14-32-30",
+        prompt="The snowflake shined under sunlight\n\n###\n\n",
+        temperature=0,
+        max_tokens=1,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        logprobs=3,
+        presence_penalty=0.0
+    )
+    result=response["choices"][0]["text"].strip()
+    
+    return result
+
+
+def getColorFromGPT(text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Please answer only the main color code that you can feel in the text. the answer should be in color code like #ddddff.\n\nSentence: "+text+"#",
+        temperature=0.7,
+        max_tokens=20,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    result=response["choices"][0]["text"].strip()
+    return result
